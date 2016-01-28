5b7f015b99d8b854ce966bb47195b36f21644d56
gitTest <703353105@qq.com>
2016-01-28 18:04:35 +0800

    第一次修改

---
+++ gitTest/gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by 苏东博 on 16/1/28.
+//  Copyright © 2016年 lanyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest/gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by 苏东博 on 16/1/28.
+//  Copyright © 2016年 lanyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
