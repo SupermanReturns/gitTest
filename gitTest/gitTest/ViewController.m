288e74c563dbb485abc92db500d60679c4ae4256
gitTest <703353105@qq.com>
2016-01-28 18:18:17 +0800

    第三次

--- gitTest/gitTest/ViewController.m
+++ gitTest/gitTest/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     UILabel *l=[UILabel new];
+    l.backgroundColor=[UIColor blackColor];
 }
 
 - (void)didReceiveMemoryWarning {
